05b51b3cbcac170df636057cf7a7176060d2d9d7
Arij Ben Belgacem <arijbenbegacem66@gmail.com>
2025-12-04 12:05:49 +0100

    Add entities, controllers, templates, and migrations

---
+++ src/Entity/Location.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LocationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LocationRepository::class)]
+class Location
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Nom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Adresse = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Ville = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $Capacite = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $Image = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $Maplien = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->Nom;
+    }
+
+    public function setNom(string $Nom): static
+    {
+        $this->Nom = $Nom;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->Adresse;
+    }
+
+    public function setAdresse(string $Adresse): static
+    {
+        $this->Adresse = $Adresse;
+
+        return $this;
+    }
+
+    public function getVille(): ?string
+    {
+        return $this->Ville;
+    }
+
+    public function setVille(string $Ville): static
+    {
+        $this->Ville = $Ville;
+
+        return $this;
+    }
+
+    public function getCapacite(): ?int
+    {
+        return $this->Capacite;
+    }
+
+    public function setCapacite(?int $Capacite): static
+    {
+        $this->Capacite = $Capacite;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->Image;
+    }
+
+    public function setImage(?string $Image): static
+    {
+        $this->Image = $Image;
+
+        return $this;
+    }
+
+    public function getMaplien(): ?string
+    {
+        return $this->Maplien;
+    }
+
+    public function setMaplien(?string $Maplien): static
+    {
+        $this->Maplien = $Maplien;
+
+        return $this;
+    }
+}
